65b26aa0aea74861950cac6de3ad82570c70cf12
Misha <misha5421203@gmail.com>
2023-10-09 00:09:06 +0300

    change save(normal file system)

--- handlers/users/img.py
+++ handlers/users/img.py
@@ -26,24 +26,33 @@ async def start_uploading_photo(call: types.CallbackQuery):
 
 URI_INFO = f"https://api.telegram.org/bot{BOT_TOKEN}/getFile?file_id="
 
+
 @dp.message_handler(content_types=["photo"], state=PhotoState.waiting_for_photo)
-async def get_photo(message:types.Message,state:FSMContext):
-
-
+async def get_photo(message: types.Message, state: FSMContext):
     URI = f"https://api.telegram.org/file/bot{BOT_TOKEN}/"
 
     file_id = message.photo[2].file_id
     resp = requests.get(URI_INFO + file_id)
     img_path = resp.json()["result"]["file_path"]
-    img = requests.get(URI+img_path)
+    img = requests.get(URI + img_path)
     img = Image.open(io.BytesIO(img.content))
-
 
     im = "images"
     if not os.path.exists("images"):
         os.mkdir("images")
-    img.save(f"images/{len(os.listdir(im))+1}.png", format="PNG")
 
+    # Получить список файлов в папке
+    files = [f for f in os.listdir(im) if os.path.isfile(os.path.join(im, f))]
+
+    # Если количество файлов превышает 10, удалить первый файл и сдвинуть остальные
+    if len(files) >= 10:
+        os.remove(os.path.join(im, files[0]))
+        for i in range(1, len(files)):
+            os.rename(os.path.join(im, files[i]), os.path.join(im, f"{i - 1}.png"))
+
+    # Сохранить новую фотографию
+    new_filename = f"images/{len(files) - 1}.png"
+    img.save(new_filename, format="PNG")
 
     await state.finish()
-    await message.answer("фотография принята!, Жду следующей!", reply_markup=AllButtons)
+    await message.answer("Фотография принята! Жду следующей!", reply_markup=AllButtons)
